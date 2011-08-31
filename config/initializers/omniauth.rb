5eeea290b7d915883532385bef93adfb5919bb89
TMaYaD <TMaYaD+github@gmail.com>
2011-08-31 22:29:28 +0530

    Removed twitter API keys from omniauth initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'qvV1ZZtFwzQVD6HGxIQDYw', 'eH6l5bHts6vynptyrgQiJbU2erIYdn9zHTDCoHhzBw'
+  # provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'
   # provider :facebook, 'APP_ID', 'APP_SECRET'
   # provider :linked_in, 'CONSUMER_KEY', 'CONSUMER_SECRET'
 end
